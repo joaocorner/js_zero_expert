7e468d5f709f63d457f82d6c8b1e4ab9aba772b1
joaocorner <joaoeduardocorner@gmail.com>
2022-08-30 18:31:59 -0300

    assignment strings and template literals

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -22,16 +22,20 @@ const isIsland = false;
 // isIsland = true;
 
 // basic operators
-console.log(population / 2);
-population++;
-console.log(population > 6);
-console.log(population < 33);
-const description =
-  country +
-  " is in " +
-  continent +
-  ", and its " +
-  population +
-  " million people speak " +
-  language;
+// console.log(population / 2);
+// population++;
+// console.log(population > 6);
+// console.log(population < 33);
+// const description =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its " +
+//   population +
+//   " million people speak " +
+//   language;
+// console.log(description);
+
+// strings and template literals
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 console.log(description);
