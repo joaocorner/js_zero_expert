07ea5eccd0d453035987a7d3bd46d7b546daef07
joaocorner <joaoeduardocorner@gmail.com>
2022-08-30 16:36:01 -0300

    assignment let, const, var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,15 +1,22 @@
 // Values and Variables
-let country = "Brazil";
-let continent = "South America";
-let population = 212;
+// let country = "Brazil";
+// let continent = "South America";
+// let population = 212;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
 // Data Types
-let isIsland = false;
+// let isIsland = false;
 let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
 console.log(typeof language);
+
+// let, const and var
+language = "portuguese";
+const country = "Brazil";
+const continent = "South America";
+const isIsland = false;
+isIsland = true;
