f3d73801602faed6a4f745e18f610d1c79b65216
joaocorner <joaoeduardocorner@gmail.com>
2022-08-30 17:19:37 -0300

    assignment basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,7 @@
 // Values and Variables
 // let country = "Brazil";
 // let continent = "South America";
-// let population = 212;
+let population = 212;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
@@ -12,11 +12,26 @@ let language;
 // console.log(typeof isIsland);
 // console.log(typeof population);
 // console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof language);
 
 // let, const and var
 language = "portuguese";
 const country = "Brazil";
 const continent = "South America";
 const isIsland = false;
-isIsland = true;
+// isIsland = true;
+
+// basic operators
+console.log(population / 2);
+population++;
+console.log(population > 6);
+console.log(population < 33);
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
