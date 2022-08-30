b6f2a6b3239a8d18598051f7b5bbf6bc2c10d2ea
joaocorner <joaoeduardocorner@gmail.com>
2022-08-30 14:43:24 -0300

    understanding variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JS is FUN!");
-
 console.log(40 + 5 - 34 + 17);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
