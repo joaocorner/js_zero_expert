75f330950cbcf3709d36ea9e700b79687dbef90c
joaocorner <joaoeduardocorner@gmail.com>
2022-08-30 15:57:37 -0300

    assignment data type

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,15 @@
+// Values and Variables
 let country = "Brazil";
 let continent = "South America";
 let population = 212;
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
