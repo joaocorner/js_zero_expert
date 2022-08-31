14ca1a4f6b1e4a5862f07085e6ed0c998c389584
joaocorner <joaoeduardocorner@gmail.com>
2022-08-31 15:32:52 -0300

    assignment logical operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -48,8 +48,19 @@ const description = `${country} is in ${continent}, and its ${population} millio
 //   );
 // }
 
-console.log("9" - "5");
-console.log("19" - "13" + "17");
-console.log("19" - "13" + 17);
-console.log("123" < 57);
-console.log(5 + 6 + "4" + 9 - 4 - 2);
+// console.log("9" - "5");
+// console.log("19" - "13" + "17");
+// console.log("19" - "13" + 17);
+// console.log("123" < 57);
+// console.log(5 + 6 + "4" + 9 - 4 - 2);
+
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
