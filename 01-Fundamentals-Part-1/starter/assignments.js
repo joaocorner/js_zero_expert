e2ceeccd9d62b4fd0e8ac9c4f5474df95b6ddb59
joaocorner <joaoeduardocorner@gmail.com>
2022-08-31 18:32:11 -0300

    assignment the conditional (ternary) operator

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -71,23 +71,27 @@ const description = `${country} is in ${continent}, and its ${population} millio
 //   console.log(`${country} does not meet your criteria :(`);
 // }
 
-switch (language) {
-  case "chinese":
-  case "mandarin":
-    console.log("MOST number of native speakers!");
-    break;
-  case "spanish":
-    console.log("2nd place in number of native speakers");
-    break;
-  case "english":
-    console.log("3rd place");
-    break;
-  case "hindi":
-    console.log("Number 4");
-    break;
-  case "arabic":
-    console.log("5th most spoken language");
-    break;
-  default:
-    console.log("Great language too :D");
-}
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too :D");
+// }
+
+console.log(
+  `${country}'s population is ${population >= 33 ? "above" : "below"} average.`
+);
