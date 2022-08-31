cf43eed34eda2ba8cc282c6fcac6811b8a726b49
joaocorner <joaoeduardocorner@gmail.com>
2022-08-31 12:29:32 -0300

    assignment type conversion and coercion

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -40,10 +40,16 @@ const isIsland = false;
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 // console.log(description);
 
-if (population > 33) {
-  console.log(`${country}'s population is above average.`);
-} else {
-  console.log(
-    `${country}'s population is ${33 - population} million below average.`
-  );
-}
+// if (population > 33) {
+//   console.log(`${country}'s population is above average.`);
+// } else {
+//   console.log(
+//     `${country}'s population is ${33 - population} million below average.`
+//   );
+// }
+
+console.log("9" - "5");
+console.log("19" - "13" + "17");
+console.log("19" - "13" + 17);
+console.log("123" < 57);
+console.log(5 + 6 + "4" + 9 - 4 - 2);
